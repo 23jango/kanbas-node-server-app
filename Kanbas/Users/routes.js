97ef71d2fa6d35484d410ac33bec77d33d7bb758
Jayden Ngo <jaydenngo618@gmail.com>
2024-12-02 23:11:57 -0500

    first a6 commit

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -3,41 +3,37 @@ import * as courseDao from "../Courses/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 // let currentUser = null;
 export default function UserRoutes(app) {
-  const createUser = (req, res) => { };
-  const deleteUser = (req, res) => { };
-  const findAllUsers = (req, res) => { };
-  const findUserById = (req, res) => { };
 
-  //make the DAO function available as a RESTful Web API. Map a route that accepts a user's primary key as a path parameter, passes the ID and request body to the DAO function and responds with the status.
-  const updateUser = (req, res) => { 
-    const userId = req.params.userId;
-    const userUpdates = req.body;
-    dao.updateUser(userId, userUpdates);
-    const currentUser = dao.findUserById(userId);
-    req.session["currentUser"] = currentUser; //If a user updates their profile, then the session must be kept in synch.
-    res.json(currentUser);
-
+  //a restful API that can make users make new users!
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
   };
 
 
-  //The signup route expects a user object with at least the properties username and password. The DAO's findUserByUsername is called to check if a user with that username already exists. If such a user is found a 400 error status is returned along with an error message for display in the user interface. If the username is not already taken the user is inserted into the database and stored in the currentUser server variable. The response includes the newly created user. The signup route is mapped to the api/users/signup path.
-  const signup = (req, res) => {
-    const user = dao.findUserByUsername(req.body.username);
+  //makes the deletuser option available from the DAO as a restful API for integration with the user interface which encodes the id of the user to remove as a path parameter.
+  const deleteUser = async (req, res) => {
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
+
+  //The signup route expects a user object with at least the properties username and password. The DAO's findUserByUsername is called to check if a user with that username already exists. If such a user is found a 400 error status is returned along with 1an error message for display in the user interface. If the username is not already taken the user is inserted into the database and stored in the currentUser server variable. The response includes the newly created user. The signup route is mapped to the api/users/signup path.
+  const signup = async (req, res) => {
+    const user = await dao.findUserByUsername(req.body.username);
     if (user) {
       res.status(400).json({ message: "Username already in use" });
       return;
     }
-    const currentUser = dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
     req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
 
-
-  const signin = (req, res) => {
+  const signin = async (req, res) => {
     console.log("Received request at /api/users/signin"); // Confirm route is hit
     console.log("Request body:", req.body); // Confirm body is parsed
     const { username, password } = req.body;
-    const currentUser = dao.findUserByCredentials(username, password);
+    const currentUser = await dao.findUserByCredentials(username, password);
 
     if (currentUser) {
       req.session["currentUser"] = currentUser;
@@ -46,7 +42,7 @@ export default function UserRoutes(app) {
       res.status(401).json({ message: "Unable to login. Try again later." });
     }
 
-    console.log(req.body)
+    console.log(req.body);
   };
 
   const profile = async (req, res) => {
@@ -60,16 +56,14 @@ export default function UserRoutes(app) {
     res.json(currentUser);
   };
 
-
-  const signout = (req, res) => {
+  const signout = async (req, res) => {
     // currentUser = null;
     req.session.destroy(); //Users can be signed out by destroying the session.
     res.sendStatus(200);
   };
 
-
   //retrieve courses in the user routes-- since enrolled courses are retrieved within the context of currently logged in user
-  const findCoursesForEnrolledUser = (req, res) => {
+  const findCoursesForEnrolledUser = async (req, res) => {
     let { userId } = req.params;
     if (userId === "current") {
       const currentUser = req.session["currentUser"];
@@ -79,27 +73,62 @@ export default function UserRoutes(app) {
       }
       userId = currentUser._id;
     }
-    const courses = courseDao.findCoursesForEnrolledUser(userId);
+    const courses = await courseDao.findCoursesForEnrolledUser(userId);
     res.json(courses);
   };
   app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
 
   //create a new course and then enroll the currentUser in the new course. Respond with the newCourse so it can be rendered in the user interface.
-  const createCourse = (req, res) => {
+  const createCourse = async (req, res) => {
     const currentUser = req.session["currentUser"];
-    const newCourse = courseDao.createCourse(req.body);
-    enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
+    const newCourse = await courseDao.createCourse(req.body);
+    await enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
     res.json(newCourse);
   };
   app.post("/api/users/current/courses", createCourse);
 
+  // - the DAO function by the same name is now made available by a restful API through here
+  const findUserById = async (req, res) => {
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+  };
 
+  //responds with a collection of users retrieved from the database
+  const findAllUsers = async (req, res) => {
+    //searching for a specific role
+    const { role, name } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
 
+    const users = await dao.findAllUsers();
+    res.json(users);
+  };
+
+  //make the DAO function available as a RESTful Web API. Map a route that accepts a user's primary key as a path parameter, passes the ID and request body to the DAO function and responds with the status.
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const userUpdates = req.body;
+    await dao.updateUser(userId, userUpdates);
+    const currentUser = req.session["currentUser"];
+    if (currentUser && currentUser._id === userId) {
+      req.session["currentUser"] = { ...currentUser, ...userUpdates };
+      //If a user updates their profile, then the session must be kept in synch.
+    }
+    res.json(currentUser);
+  };
+  app.put("/api/users/:userId", updateUser);
 
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
-  app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
   app.post("/api/users/signup", signup);
   app.post("/api/users/signin", signin);
